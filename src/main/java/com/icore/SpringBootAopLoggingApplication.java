190d39b3833f8b89ebf6715962c1871786bbf08b
sowmya <potusowmya.sow@gmail.com>
2020-12-07 15:15:10 +0530

    first commit

---
+++ src/main/java/com/icore/SpringBootAopLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.icore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopLoggingApplication.class, args);
+	}
+
+}
